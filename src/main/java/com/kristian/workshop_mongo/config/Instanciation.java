1453dc321592220e78850b2fe477aa61a6e6f765
Kristian <kristianlll@hotmail.com>
2025-12-19 16:00:17 -0300

    Seeding do db

---
+++ src/main/java/com/kristian/workshop_mongo/config/Instanciation.java
@@ -0,0 +1,31 @@
+package com.kristian.workshop_mongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.kristian.workshop_mongo.domain.User;
+import com.kristian.workshop_mongo.repository.UserRepository;
+
+@Configuration
+public class Instanciation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(alex, maria, bob));
+
+	}
+
+}
