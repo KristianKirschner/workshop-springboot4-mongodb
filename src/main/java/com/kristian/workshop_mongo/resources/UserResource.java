fe7ec09a5ba2280182a0c9be18bcae41ecc07a29
Kristian <kristianlll@hotmail.com>
2025-12-22 16:46:04 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/kristian/workshop_mongo/resources/UserResource.java
+++ src/main/java/com/kristian/workshop_mongo/resources/UserResource.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.kristian.workshop_mongo.DTO.UserDTO;
+import com.kristian.workshop_mongo.domain.Post;
 import com.kristian.workshop_mongo.domain.User;
 import com.kristian.workshop_mongo.services.UserService;
 
@@ -60,6 +61,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
